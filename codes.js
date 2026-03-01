7d785cf824b37b31c722f1ea4a55ed938774afb6
unknown <pinkunaprusty108@gmail.com>
2026-03-01 17:16:26 +0530

    first commit

---
+++ codes.js
@@ -0,0 +1,14 @@
+const countryList = {
+  USD: "US",
+  INR: "IN",
+  EUR: "FR",
+  GBP: "GB",
+  AUD: "AU",
+  CAD: "CA",
+  JPY: "JP",
+  CNY: "CN",
+  NZD: "NZ",
+  CHF: "CH",
+  IDR: "ID",
+  TOP: "TO"
+};
